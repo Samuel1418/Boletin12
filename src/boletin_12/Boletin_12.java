1cb1f97c671afa601284c18ff6d9f678dad011c0
Samuel1418 <sgomezpintos@danielcastelao.org>
2018-11-28 20:09:22 +0100

    Garaje listo

---
+++ src/boletin_12/Boletin_12.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_12;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Boletin_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Garaje obx = new Garaje();
+        obx.programa();
+    }
+    
+}
